8876891cabebcd182a681741e108c782b77326bb
Julian Lam <julian@nodebb.org>
2017-11-07 12:06:56 -0500

    removed debug log and fixed route to admin page

--- static/lib/main.js
+++ static/lib/main.js
@@ -77,7 +77,6 @@ $(document).ready(function () {
 	};
 
 	Tenor.select = function (textarea, selectionStart, selectionEnd, url) {
-		console.log('selecting', arguments);
 		require([
 			'composer/formatting',
 			'composer/controls',
