689b26eff240b3f6fd1eda4993b465ccc8423137
Julian Lam <julian@nodebb.org>
2018-02-26 12:18:53 -0500

    Fixes #1, Fixes #2
    - Tenor Gif plugin now works with Redactor
    - The alt text is preset to the search query now

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* globals document, window, socket, bootbox */
+/* globals $, document, window, socket, bootbox */
 
 $(document).ready(function () {
 	var Tenor = {};
@@ -9,64 +9,49 @@ $(document).ready(function () {
 		Tenor.prepareFormattingTools();
 	});
 
+	$(window).on('action:redactor.load', function () {
+		Tenor.initRedactor.apply(this, arguments);
+	});
+
+	$(window).on('action:composer.loaded', function () {
+		$.Redactor.opts.plugins.push('tenor-gif');
+	});
+
 	Tenor.prepareFormattingTools = function () {
 		require([
 			'composer/formatting',
 			'composer/controls',
-			'translator',
-			'benchpress',
-		], function (formatting, controls, translator, Benchpress) {
+		], function (formatting, controls) {
 			if (formatting && controls) {
 				formatting.addButtonDispatch('gif', function (textarea, selectionStart, selectionEnd) {
-					Benchpress.parse('plugins/tenor-gif/modal', {}, function (html) {
-						var modal = bootbox.dialog({
-							title: 'Insert GIF',
-							message: html,
-							className: 'tenor-gif-modal',
-							onEscape: true,
-						});
-
-						var queryEl = modal.find('#gif-query');
-						var resultsEl = modal.find('#gif-results');
-						var queryTimeout;
-
-						modal.on('shown.bs.modal', function () {
-							queryEl.focus();
-						});
-
-						queryEl.on('keyup', function () {
-							if (!queryEl.val().length) {
-								return;
-							}
-
-							if (queryTimeout) {
-								clearTimeout(queryTimeout);
-							}
-
-							queryTimeout = setTimeout(function () {
-								socket.emit('plugins.tenor-gif.query', {
-									query: queryEl.val(),
-								}, function (err, gifs) {
-									if (err) {
-										return translator.translate(err.message, function (translated) {
-											resultsEl.addClass('alert alert-warning').text(translated);
-										});
-									}
-
-									Tenor.populateDOM(resultsEl, gifs);
-								});
-							}, 250);
-						});
-
-						resultsEl.on('click', 'img[data-url]', function () {
-							var url = this.getAttribute('data-url');
-							Tenor.select(textarea, selectionStart, selectionEnd, url);
-							modal.modal('hide');
-						});
+					Tenor.showModal(function (url, query) {
+						Tenor.select(textarea, selectionStart, selectionEnd, url, query);
 					});
 				});
 			}
 		});
+	};
+
+	Tenor.initRedactor = function () {
+		$.Redactor.prototype['tenor-gif'] = function () {
+			return {
+				init: function () {
+					var self = this;
+					var button = self.button.add('tenor-gif', 'Insert GIF');
+					self.button.setIcon(button, '<i class="fa fa-tenor-gif"></i>');
+					self.button.addCallback(button, self['tenor-gif'].onClick);
+				},
+				onClick: function () {
+					var self = this;
+					Tenor.showModal(function (url, query) {
+						var code = self.code.get();
+						code += '<p><img src="' + url + '" alt="' + query + '" /></p>';
+
+						self.code.set(code);
+					});
+				},
+			};
+		};
 	};
 
 	Tenor.populateDOM = function (resultsEl, gifs) {
@@ -79,7 +64,57 @@ $(document).ready(function () {
 		});
 	};
 
-	Tenor.select = function (textarea, selectionStart, selectionEnd, url) {
+	Tenor.showModal = function (callback) {
+		require(['translator', 'benchpress'], function (translator, Benchpress) {
+			Benchpress.parse('plugins/tenor-gif/modal', {}, function (html) {
+				var modal = bootbox.dialog({
+					title: 'Insert GIF',
+					message: html,
+					className: 'tenor-gif-modal',
+					onEscape: true,
+				});
+
+				var queryEl = modal.find('#gif-query');
+				var resultsEl = modal.find('#gif-results');
+				var queryTimeout;
+
+				modal.on('shown.bs.modal', function () {
+					queryEl.focus();
+				});
+
+				queryEl.on('keyup', function () {
+					if (!queryEl.val().length) {
+						return;
+					}
+
+					if (queryTimeout) {
+						clearTimeout(queryTimeout);
+					}
+
+					queryTimeout = setTimeout(function () {
+						socket.emit('plugins.tenor-gif.query', {
+							query: queryEl.val(),
+						}, function (err, gifs) {
+							if (err) {
+								return translator.translate(err.message, function (translated) {
+									resultsEl.addClass('alert alert-warning').text(translated);
+								});
+							}
+
+							Tenor.populateDOM(resultsEl, gifs);
+						});
+					}, 250);
+				});
+
+				resultsEl.on('click', 'img[data-url]', function () {
+					callback(this.getAttribute('data-url'), queryEl.val());
+					modal.modal('hide');
+				});
+			});
+		});
+	};
+
+	Tenor.select = function (textarea, selectionStart, selectionEnd, url, query) {
 		require([
 			'composer/formatting',
 			'composer/controls',
@@ -87,8 +122,8 @@ $(document).ready(function () {
 		], function (formatting, controls, translator) {
 			translator.getTranslations(window.config.userLang || window.config.defaultLang, 'markdown', function (strings) {
 				if (selectionStart === selectionEnd) {
-					controls.insertIntoTextarea(textarea, '![' + strings.picture_text + '](' + url + ')');
-					controls.updateTextareaSelection(textarea, selectionStart + strings.picture_text.length + 4, selectionEnd + strings.picture_text.length + url.length + 4);
+					controls.insertIntoTextarea(textarea, '![' + query + '](' + url + ')');
+					controls.updateTextareaSelection(textarea, selectionStart + query.length + 4, selectionEnd + query.length + url.length + 4);
 				} else {
 					var wrapDelta = controls.wrapSelectionInTextareaWith(textarea, '![', '](' + url + ')');
 					controls.updateTextareaSelection(textarea, selectionEnd + 4 - wrapDelta[1], selectionEnd + url.length + 4 - wrapDelta[1]);
